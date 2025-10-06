cd416a00172de8067a4d5449b1659f8fade2e5b2
Mio Mioka <fkym20010123@gmail.com>
2025-10-06 20:35:10 +0900

    Add health_check page for debugging

--- race/views.py
+++ race/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+# race/views.py
+from django.http import HttpResponse
+
+# ... ファイルの末尾に追加 ...
+def health_check(request):
+    return HttpResponse("Hello, Vercel! Django is running correctly.")
